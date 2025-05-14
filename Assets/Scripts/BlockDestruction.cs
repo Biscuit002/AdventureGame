f23884046037af46f01d6d93957858fee4f638ea
Biscuit002 <aplatosh6982@student.pps.net>
2025-05-14 15:29:00 -0700

    INVENTORY

--- Assets/Scripts/BlockDestruction.cs
+++ Assets/Scripts/BlockDestruction.cs
@@ -1,32 +1,77 @@
 using System.Collections.Concurrent;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BlockDestruction : MonoBehaviour
 {
     private float mouseX;
     private float mouseY;
-    public GameObject grassPrefab;
-    public GameObject dirtPrefab;
-    public GameObject stonePrefab;
-    public GameObject woodPrefab;
-    public GameObject leafPrefab;
-    public GameObject copperPrefab;
-    public GameObject diamondPrefab;
-    public GameObject goldPrefab;
-    public GameObject ironPrefab;
-    public GameObject platinumPrefab;
-    public GameObject silverPrefab;
-    public GameObject titaniumPrefab;
+    public GameObject[] blockPrefabs;
     private float blockInteractionDistance = 5f;
     public Inventory inventory;
+    public int blockIndex;
     private void Start()
     {
         inventory = FindObjectOfType<Inventory>();
+
     }
     void Update()
     {
         mouseX = Input.GetAxis("Mouse X");
         mouseY = Input.GetAxis("Mouse Y");
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            blockIndex = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            blockIndex = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            blockIndex = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            blockIndex = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            blockIndex = 4;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            blockIndex = 5;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            blockIndex = 6;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            blockIndex = 7;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            blockIndex = 8;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            blockIndex = 9;
+        }
+        if (Input.GetKeyDown(KeyCode.Minus))
+        {
+            blockIndex = 10;
+        }
+        if (Input.GetKeyDown(KeyCode.Equals))
+        {
+            blockIndex = 11;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftBracket))
+        {
+            blockIndex = 12;
+        }
 
         if (Input.GetMouseButtonDown(0))
         {
@@ -34,11 +79,65 @@ public class BlockDestruction : MonoBehaviour
         }
         if (Input.GetMouseButtonDown(1))
         {
-            placeBlock();
+            for (int i = 0; i < blockPrefabs.Length; i++)
+            {
+                if (i == blockIndex && inventory.currentBlockAmount > 0)
+                {
+                    placeBlock(blockPrefabs[i]);
+                }
+            }
+            if (blockIndex == 0 && inventory.grassAmount > 0)
+            {
+                inventory.grassAmount--;
+            }
+            else if (blockIndex == 1 && inventory.dirtAmount > 0)
+            {
+                inventory.dirtAmount--;
+            }
+            else if (blockIndex == 2 && inventory.stoneAmount > 0)
+            {
+                inventory.stoneAmount--;
+            }
+            else if (blockIndex == 3 && inventory.woodAmount > 0)
+            {
+                inventory.woodAmount--;
+            }
+            else if (blockIndex == 4 && inventory.leafAmount > 0)
+            {
+                inventory.leafAmount--;
+            }
+            else if (blockIndex == 5 && inventory.copperAmount > 0)
+            {
+                inventory.copperAmount--;
+            }
+            else if (blockIndex == 6 && inventory.diamondAmount > 0)
+            {
+                inventory.diamondAmount--;
+            }
+            else if (blockIndex == 7 && inventory.goldAmount > 0)
+            {
+                inventory.goldAmount--;
+            }
+            else if (blockIndex == 8 && inventory.ironAmount > 0)
+            {
+                inventory.ironAmount--;
+            }
+            else if (blockIndex == 9 && inventory.platinumAmount > 0)
+            {
+                inventory.platinumAmount--;
+            }
+            else if (blockIndex == 10 && inventory.silverAmount > 0)
+            {
+                inventory.silverAmount--;
+            }
+            else if (blockIndex == 11 && inventory.titaniumAmount > 0)
+            {
+                inventory.titaniumAmount--;
+            }
         }
     }
 
-    private void placeBlock()
+    private void placeBlock(GameObject blockType)
     {
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 mousePos2D = new Vector2(mouseWorldPos.x, mouseWorldPos.y);
@@ -50,7 +149,7 @@ public class BlockDestruction : MonoBehaviour
             mousePosition.z = 0;
 
             mousePosition = new Vector3(Mathf.Round(mousePosition.x), Mathf.Round(mousePosition.y), -0.01f);
-            Instantiate(grassPrefab, mousePosition, Quaternion.identity);
+            Instantiate(blockType, mousePosition, Quaternion.identity);
         }
     }
 
--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -3,6 +3,9 @@ using TMPro;
 
 public class Inventory : MonoBehaviour
 {
+    private Vector3 targetScale = new Vector3(1.2f, 1.2f, 1.2f);
+    private float speed = 5f;
+
     public int grassAmount = 0;
     public int dirtAmount = 0;
     public int stoneAmount = 0;
@@ -15,6 +18,8 @@ public class Inventory : MonoBehaviour
     public int platinumAmount = 0;
     public int silverAmount = 0;
     public int titaniumAmount = 0;
+
+    public int currentBlockAmount;
 
     public TextMeshProUGUI grassText;
     public TextMeshProUGUI dirtText;
@@ -29,10 +34,25 @@ public class Inventory : MonoBehaviour
     public TextMeshProUGUI silverText;
     public TextMeshProUGUI titaniumText;
 
+    public GameObject grassUI;
+    public GameObject dirtUI;
+    public GameObject stoneUI;
+    public GameObject woodUI;
+    public GameObject leafUI;
+    public GameObject copperUI;
+    public GameObject diamondUI;
+    public GameObject goldUI;
+    public GameObject ironUI;
+    public GameObject platinumUI;
+    public GameObject silverUI;
+    public GameObject titaniumUI;
+
+    public BlockDestruction blockDestruction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        blockDestruction = FindObjectOfType<BlockDestruction>();
     }
 
     // Update is called once per frame
@@ -50,5 +70,233 @@ public class Inventory : MonoBehaviour
         platinumText.text = "" + platinumAmount;
         silverText.text = "" + silverAmount;
         titaniumText.text = "" + titaniumAmount;
+
+        if (blockDestruction.blockIndex == 0)
+        {
+            currentBlockAmount = grassAmount;
+            grassUI.transform.localScale = Vector3.Lerp(grassUI.transform.localScale, targetScale, Time.deltaTime * speed);
+        }
+        else
+        {
+            grassUI.transform.localScale = Vector3.Lerp(grassUI.transform.localScale, new Vector3(1f, 1f, 1f), Time.deltaTime * speed);
+        }
+
+        if (blockDestruction.blockIndex == 1)
+        {
+            currentBlockAmount = dirtAmount;
+            dirtUI.transform.localScale = Vector3.Lerp(dirtUI.transform.localScale, targetScale, Time.deltaTime * speed);
+        }
+        else
+        {
+            dirtUI.transform.localScale = Vector3.Lerp(dirtUI.transform.localScale, new Vector3(1f, 1f, 1f), Time.deltaTime * speed);
+        }
+
+        if (blockDestruction.blockIndex == 2)
+        {
+            currentBlockAmount = stoneAmount;
+            stoneUI.transform.localScale = Vector3.Lerp(stoneUI.transform.localScale, targetScale, Time.deltaTime * speed);
+        }
+        else
+        {
+            stoneUI.transform.localScale = Vector3.Lerp(stoneUI.transform.localScale, new Vector3(1f, 1f, 1f), Time.deltaTime * speed);
+        }
+
+        if (blockDestruction.blockIndex == 3)
+        {
+            currentBlockAmount = woodAmount;
+            woodUI.transform.localScale = Vector3.Lerp(woodUI.transform.localScale, targetScale, Time.deltaTime * speed);
+        }
+        else
+        {
+            woodUI.transform.localScale = Vector3.Lerp(woodUI.transform.localScale, new Vector3(1f, 1f, 1f), Time.deltaTime * speed);
+        }
+
+        if (blockDestruction.blockIndex == 4)
+        {
+            currentBlockAmount = leafAmount;
+            leafUI.transform.localScale = Vector3.Lerp(leafUI.transform.localScale, targetScale, Time.deltaTime * speed);
+        }
+        else
+        {
+            leafUI.transform.localScale = Vector3.Lerp(leafUI.transform.localScale, new Vector3(1f, 1f, 1f), Time.deltaTime * speed);
+        }
+
+        if (blockDestruction.blockIndex == speed)
+        {
+            currentBlockAmount = copperAmount;
+            copperUI.transform.localScale = Vector3.Lerp(copperUI.transform.localScale, targetScale, Time.deltaTime * speed);
+        }
+        else
+        {
+            copperUI.transform.localScale = Vector3.Lerp(copperUI.transform.localScale, new Vector3(1f, 1f, 1f), Time.deltaTime * speed);
+        }
+
+        if (blockDestruction.blockIndex == 6)
+        {
+            currentBlockAmount = diamondAmount;
+            diamondUI.transform.localScale = Vector3.Lerp(diamondUI.transform.localScale, targetScale, Time.deltaTime * speed);
+        }
+        else
+        {
+            diamondUI.transform.localScale = Vector3.Lerp(diamondUI.transform.localScale, new Vector3(1f, 1f, 1f), Time.deltaTime * speed);
+        }
+
+        if (blockDestruction.blockIndex == 7)
+        {
+            currentBlockAmount = goldAmount;
+            goldUI.transform.localScale = Vector3.Lerp(goldUI.transform.localScale, targetScale, Time.deltaTime * speed);
+        }
+        else
+        {
+            goldUI.transform.localScale = Vector3.Lerp(goldUI.transform.localScale, new Vector3(1f, 1f, 1f), Time.deltaTime * speed);
+        }
+
+        if (blockDestruction.blockIndex == 8)
+        {
+            currentBlockAmount = ironAmount;
+            ironUI.transform.localScale = Vector3.Lerp(ironUI.transform.localScale, targetScale, Time.deltaTime * speed);
+        }
+        else
+        {
+            ironUI.transform.localScale = Vector3.Lerp(ironUI.transform.localScale, new Vector3(1f, 1f, 1f), Time.deltaTime * speed);
+        }
+
+        if (blockDestruction.blockIndex == 9)
+        {
+            currentBlockAmount = platinumAmount;
+            platinumUI.transform.localScale = Vector3.Lerp(platinumUI.transform.localScale, targetScale, Time.deltaTime * speed);
+        }
+        else
+        {
+            platinumUI.transform.localScale = Vector3.Lerp(platinumUI.transform.localScale, new Vector3(1f, 1f, 1f), Time.deltaTime * speed);
+        }
+
+        if (blockDestruction.blockIndex == 10)
+        {
+            currentBlockAmount = silverAmount; 
+            silverUI.transform.localScale = Vector3.Lerp(silverUI.transform.localScale, targetScale, Time.deltaTime * speed);
+        }
+        else
+        {
+            silverUI.transform.localScale = Vector3.Lerp(silverUI.transform.localScale, new Vector3(1f, 1f, 1f), Time.deltaTime * speed);
+        }
+
+        if (blockDestruction.blockIndex == 11)
+        {
+            currentBlockAmount = titaniumAmount;
+            titaniumUI.transform.localScale = Vector3.Lerp(titaniumUI.transform.localScale, targetScale, Time.deltaTime * speed);
+        }
+        else
+        {
+            titaniumUI.transform.localScale = Vector3.Lerp(titaniumUI.transform.localScale, new Vector3(1f, 1f, 1f), Time.deltaTime * speed);
+        }
+
+        if (grassAmount > 0)
+        {
+            grassUI.SetActive(true);
+        }
+        else
+        {
+            grassUI.SetActive(false);
+        }
+
+        if (dirtAmount > 0)
+        {
+            dirtUI.SetActive(true);
+        }
+        else
+        {
+            dirtUI.SetActive(false);
+        }
+
+        if (stoneAmount > 0)
+        {
+            stoneUI.SetActive(true);
+        }
+        else
+        {
+            stoneUI.SetActive(false);
+        }
+
+        if (woodAmount > 0)
+        {
+            woodUI.SetActive(true);
+        }
+        else
+        {
+            woodUI.SetActive(false);
+        }
+
+        if (leafAmount > 0)
+        {
+            leafUI.SetActive(true);
+        }
+        else
+        {
+            leafUI.SetActive(false);
+        }
+
+        if (copperAmount > 0)
+        {
+            copperUI.SetActive(true);
+        }
+        else
+        {
+            copperUI.SetActive(false);
+        }
+
+        if (diamondAmount > 0)
+        {
+            diamondUI.SetActive(true);
+        }
+        else
+        {
+            diamondUI.SetActive(false);
+        }
+
+        if (goldAmount > 0)
+        {
+            goldUI.SetActive(true);
+        }
+        else
+        {
+            goldUI.SetActive(false);
+        }
+
+        if (ironAmount > 0)
+        {
+            ironUI.SetActive(true);
+        }
+        else
+        {
+            ironUI.SetActive(false);
+        }
+
+        if (platinumAmount > 0)
+        {
+            platinumUI.SetActive(true);
+        }
+        else
+        {
+            platinumUI.SetActive(false);
+        }
+
+        if (silverAmount > 0)
+        {
+            silverUI.SetActive(true);
+        }
+        else
+        {
+            silverUI.SetActive(false);
+        }
+
+        if (titaniumAmount > 0)
+        {
+            titaniumUI.SetActive(true);
+        }
+        else
+        {
+            titaniumUI.SetActive(false);
+        }
     }
 }
